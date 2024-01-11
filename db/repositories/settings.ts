b6914c774843ff9e75ee6e354ab19ea16a29e870
Brett Cocking <brett@trademarktechnology.co.za>
2024-01-11 12:48:53 +0200

    Add settings routes

---
+++ db/repositories/settings.ts
@@ -0,0 +1,15 @@
+import { eq } from 'drizzle-orm';
+import { Setting, settings } from '@/db/schema/settings';
+import { db } from '@/lib/db';
+
+export async function getSettings() {
+  const allSettings = await db.select().from(settings);
+  return allSettings.reduce((acc: { [key: string]: string }, setting) => {
+    acc[setting.name] = setting.value;
+    return acc;
+  }, {});
+}
+
+export async function updateSetting(name: string, value: string) {
+  return db.update(settings).set({ value }).where(eq(settings.name, name));
+}
