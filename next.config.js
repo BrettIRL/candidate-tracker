72d09868ec30ff43f0d9d5631f55cd6a5112e65a
Brett Cocking <brett@trademarktechnology.co.za>
2024-02-07 11:33:51 +0200

    Add url rewrite for assessment

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/a/:opportunityId',
+        destination: '/assessment/:opportunityId',
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
